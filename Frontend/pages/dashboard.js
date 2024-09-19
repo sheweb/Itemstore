f72175d3dbbb2b34b6fd2a4ff4162e77c9c3b1a5
shemuskan <vikashkumaryadav17april2002@gmail.com>
2024-09-19 22:07:39 +0530

    Chnaged to cfx

--- Frontend/pages/dashboard.js
+++ Frontend/pages/dashboard.js
@@ -61,7 +61,7 @@ export default function CreatorDashboard() {
               <div key={i} className="border shadow rounded-xl overflow-hidden">
                 <img src={nft.image} className="rounded" />
                 <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">Price - {nft.price} ETH</p>
+                  <p className="text-2xl font-bold text-white">Price - {nft.price} cfx</p>
                 </div>
               
               </div>
